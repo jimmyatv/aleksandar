f9549f7d8f74942599c277f157932dfa0f167cba
jimmyatv <rasic.alexandar@gmail.com>
2024-01-16 21:22:11 +0100

    Starter , let's role

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import css from './Header.module.scss';
+import React from "react";
+import css from "./Header.module.scss";
+import { MdPhoneInTalk } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div className={css.wrapper}>
-      
-      <div className={css.container}>
-        <div className={css.name}>
-          Alex
-        </div>
+    <div className={`paddings ${css.wrapper}`}>
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={css.name}>Alex</div>
+
+        <ul className={`flexCenter ${css.menu}`}>
+          <li>
+            <a href="/">Services</a>
+          </li>
+          <li>
+            <a href="/">Experience</a>
+          </li>
+          <li>
+            <a href="/">Portfolio</a>
+          </li>
+          <li>
+            <a href="/">Projects</a>
+          </li>
+          <li>
+            <p>+381606546008</p>
+            <MdPhoneInTalk size={"40px"} />
+          </li>
+        </ul>
       </div>
-      
     </div>
-  )
+  );
 };
 
 export default Header;
