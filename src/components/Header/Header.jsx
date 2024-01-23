d1642c90d52e7b06090ba9fadc9a6b4b90e9f2d6
jimmyatv <rasic.alexandar@gmail.com>
2024-01-23 11:48:56 +0100

    Burger menu | ADDED

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,49 @@
-import React from "react";
-import css from "./Header.module.scss";
+import React, { useState } from 'react';
+import css from './Header.module.scss';
 import { MdPhoneInTalk } from "react-icons/md";
+import { BiMenuAltRight } from "react-icons/bi";
+import { motion } from 'framer-motion';
+import { getMenuStyles, headerVariants } from '../../utils/motion';
 
 const Header = () => {
-  return (
-    <div className={`paddings ${css.wrapper}`}>
-      <div className={`flexCenter innerWidth ${css.container}`}>
-        <div className={css.name}>Alex</div>
 
-        <ul className={`flexCenter ${css.menu}`}>
-          <li>
-            <a href="/">Services</a>
-          </li>
-          <li>
-            <a href="/">Experience</a>
-          </li>
-          <li>
-            <a href="/">Portfolio</a>
-          </li>
-          <li>
-            <a href="/">Projects</a>
-          </li>
-          <li>
+  const [menuOpened, setMenuOpened] = useState(false);
+  const handledMenu = () => {
+    setMenuOpened(!menuOpened)
+  }
+
+  return (
+    <motion.div
+      initial='hidden'
+      whileInView='show'
+      variants={headerVariants}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}>
+
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={css.name}>
+          Alex
+        </div>
+
+        <ul
+        style={getMenuStyles(menuOpened)}
+        className={`flexCenter ${css.menu}`}>
+          <li><a href="/">Services</a></li>
+          <li><a href="/">Experience</a></li>
+          <li><a href="/">Portfolio</a></li>
+          <li><a href="/">Projects</a></li>
+          <li className={`flexCenter ${css.phone}`}>
             <p>+381606546008</p>
-            <MdPhoneInTalk size={"40px"} />
+            <MdPhoneInTalk size={'40px'} />
           </li>
         </ul>
+{/* Only for media screen */}
+        <div onClick={handledMenu} className={css.menuIcon}>
+          <BiMenuAltRight size={'40px'} />
+        </div>
       </div>
-    </div>
-  );
+    </motion.div>
+  )
 };
 
 export default Header;
