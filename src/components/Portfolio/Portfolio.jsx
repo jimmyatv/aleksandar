406dda329356c561d4ea0887e1f0b4ed98f6841c
jimmyatv <rasic.alexandar@gmail.com>
2024-02-02 11:01:42 +0100

    Portfolio framer motion | ADDED

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,43 +2,50 @@ import React from 'react';
 import css from './Portfolio.module.scss';
 import { FaHeart } from "react-icons/fa";
 import {motion} from 'framer-motion';
-import {fadeIn} from '../../utils/motion';
+import { fadeIn, staggerChildren, textVariant } from '../../utils/motion';
 import project1 from '../../assets/project1.png';
 import project2 from '../../assets/project2.png';
 import project3 from '../../assets/project3.png';
 
 const Portfolio = () => {
     return (
-        <section className={`paddings ${css.wrapper}`}>
+        <motion.section
+        variants={staggerChildren}
+        initial='hidden'
+        whileInView='show'
+        viewport={{once: false, amount: 0.25}}
+        className={`paddings ${css.wrapper}`}>
             <div className={`innerWidth flexCenter ${css.container}`}>
                 <div className={css.heading}>
-                    <div>
+                    <motion.div
+                    variants={textVariant(0.5)}
+                    >
                         <span className='primaryText'>
                             My Works
                         </span>
                         <p className='secondaryText' style={{ marginTop: '10px' }}>
                             Build with <FaHeart /> by Aleksandar Rasic
                         </p>
-                    </div>
+                    </motion.div>
 
                     {/* Projects */}
                     <div className={css.cta}>
                         <motion.div 
-                        variants={fadeIn('up', 'tween', 0.5, 0.6)}
+                        variants={fadeIn('up', 'easeOut', 0.5, 0.6)}
                         >
                             <a target='_blank' href="https://jimmyatv.github.io/myBabySpa"><img src={project1} alt="" /></a>
                             <p>React project</p>
                         </motion.div>
 
                         <motion.div
-                        variants={fadeIn('up', 'tween', 0.7, 0.6)}
+                        variants={fadeIn('up', 'easeOut', 0.7, 0.6)}
                         >
                             <a target='_blank' href="https://jimmyatv.github.io/Ramen"><img src={project2} alt="" /></a>
                             <p>Bootstrap and Vanilla JS</p>
                         </motion.div>
 
                         <motion.div
-                        variants={fadeIn('up', 'tween', 0.9, 0.6)}
+                        variants={fadeIn('up', 'easeOut', 0.9, 0.6)}
                         >
                             <a target='_blank' href="https://jimmyatv.github.io/OwnItAll-byAlex"><img src={project3} alt="" /></a>
                             <p>Basic HTML and CSS</p>
@@ -46,7 +53,7 @@ const Portfolio = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     )
 };
 
